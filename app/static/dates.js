578a2d7fe08ce1f607921abbb5e9b29ce8950e22
AlexandreBoult <alex.boult@orange.fr>
2025-03-18 16:23:55 +0100

    bug fix

---
+++ app/static/dates.js
@@ -0,0 +1,12 @@
+
+
+function calcul() {
+    const date1 = new Date(document.getElementById('date1').value);
+    const date2 = new Date(document.getElementById('date2').value);
+    console.log(document.getElementById('date1').value);
+    const difference = date1.getTime() - date2.getTime();
+    const TotalDiffDays = Math.ceil(difference / (1000 * 3600 * 24));
+    document.getElementById('resultat').innerHTML=TotalDiffDays + ' jours';
+}
+
+document.getElementById('boutton').addEventListener('mousedown',calcul);
--- app/static/script.js
+++ app/static/script.js
@@ -1,39 +1,40 @@
 var taux = 0;
 var port = 0;
 var temps_travail=1;
+var prime=1;
 
 function horaire_brut() {
   const horaire_brut=document.getElementById("horaire_brut").value;
   document.getElementById("horaire_net").value=Math.round(horaire_brut*(1-port/100));
   document.getElementById("mensuel_brut").value=Math.round(horaire_brut*151.667*temps_travail);
-  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail)*12;
+  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail*prime)*12;
   const horaire_net=document.getElementById("horaire_net").value;
   document.getElementById("mensuel_net").value=Math.round(horaire_net*151.667*temps_travail);
-  document.getElementById("annuel_net").value=Math.round(horaire_net*151.667*temps_travail)*12;
+  document.getElementById("annuel_net").value=Math.round(horaire_net*151.667*temps_travail*prime)*12;
   calcul_taux()
   }
 
 function mensuel_brut() {
   const mensuel_brut=document.getElementById("mensuel_brut").value;
   document.getElementById("horaire_brut").value=parseFloat((mensuel_brut/(151.667*temps_travail)).toFixed(2));
-  document.getElementById("annuel_brut").value=mensuel_brut*12;
+  document.getElementById("annuel_brut").value=Math.round(mensuel_brut*12*prime);
   const horaire_brut=document.getElementById("horaire_brut").value;
   document.getElementById("horaire_net").value=Math.round(horaire_brut*(1-port/100));
   const horaire_net=document.getElementById("horaire_net").value;
   document.getElementById("mensuel_net").value=Math.round(horaire_net*temps_travail*151.667);
-  document.getElementById("annuel_net").value=Math.round(horaire_net*temps_travail*151.667)*12;
+  document.getElementById("annuel_net").value=Math.round(horaire_net*temps_travail*151.667*prime)*12;
   calcul_taux()
   }
 
 function annuel_brut() {
   const annuel_brut=document.getElementById("annuel_brut").value;
-  document.getElementById("mensuel_brut").value=Math.round(annuel_brut/12);
-  document.getElementById("horaire_brut").value=parseFloat((annuel_brut/(12*151.667*temps_travail)).toFixed(2));
+  document.getElementById("mensuel_brut").value=Math.round(annuel_brut/(12*prime));
+  document.getElementById("horaire_brut").value=parseFloat((annuel_brut/(12*151.667*temps_travail*prime)).toFixed(2));
   const horaire_brut=document.getElementById("horaire_brut").value;
   document.getElementById("horaire_net").value=Math.round(horaire_brut*(1-port/100));
   const horaire_net=document.getElementById("horaire_net").value;
   document.getElementById("mensuel_net").value=Math.round(horaire_net*temps_travail*151.667);
-  document.getElementById("annuel_net").value=Math.round(horaire_net*temps_travail*151.667)*12;
+  document.getElementById("annuel_net").value=Math.round(horaire_net*temps_travail*151.667*prime)*12;
   calcul_taux()
   }
 
@@ -41,33 +42,33 @@ function horaire_net() {
   const horaire_net=document.getElementById("horaire_net").value;
   document.getElementById("horaire_brut").value=Math.round(horaire_net/(1-port/100));
   document.getElementById("mensuel_net").value=Math.round(horaire_net*151.667*temps_travail);
-  document.getElementById("annuel_net").value=Math.round(horaire_net*151.667*temps_travail)*12;
+  document.getElementById("annuel_net").value=Math.round(horaire_net*151.667*temps_travail*prime)*12;
   const horaire_brut=document.getElementById("horaire_brut").value;
   document.getElementById("mensuel_brut").value=Math.round(horaire_brut*151.667*temps_travail);
-  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail)*12;
+  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail*prime)*12;
   calcul_taux()
   }
 
 function mensuel_net() {
   const mensuel_net=document.getElementById("mensuel_net").value;
   document.getElementById("horaire_net").value=parseFloat((mensuel_net/(151.667*temps_travail)).toFixed(2));
-  document.getElementById("annuel_net").value=mensuel_net*12;
+  document.getElementById("annuel_net").value=Math.round(mensuel_net*12*prime);
   document.getElementById("mensuel_brut").value=Math.round(mensuel_net/(1-port/100));
   const mensuel_brut=document.getElementById("mensuel_brut").value;
   document.getElementById("horaire_brut").value=parseFloat((mensuel_brut/(151.667*temps_travail)).toFixed(2));
-  document.getElementById("annuel_brut").value=mensuel_brut*12;
+  document.getElementById("annuel_brut").value=Math.round(mensuel_brut*12*prime);
   calcul_taux()
   }
 
 function annuel_net() {
   const annuel_net=document.getElementById("annuel_net").value;
-  document.getElementById("mensuel_net").value=Math.round(annuel_net/12);
-  document.getElementById("horaire_net").value=parseFloat((annuel_net/(12*151.667*temps_travail)).toFixed(2));
+  document.getElementById("mensuel_net").value=Math.round(annuel_net/(12*prime));
+  document.getElementById("horaire_net").value=parseFloat((annuel_net/(12*151.667*temps_travail*prime)).toFixed(2));
   const horaire_net=document.getElementById("horaire_net").value;
   document.getElementById("horaire_brut").value=Math.round(horaire_net/(1-port/100));
   const horaire_brut=document.getElementById("horaire_brut").value;
   document.getElementById("mensuel_brut").value=Math.round(horaire_brut*151.667*temps_travail);
-  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail)*12;
+  document.getElementById("annuel_brut").value=Math.round(horaire_brut*151.667*temps_travail*prime)*12;
   calcul_taux()
   }
 
@@ -97,6 +98,7 @@ function fn_statut() {
 
 function calcul_taux() {
     const mensuel_net=document.getElementById("mensuel_net").value
+    const annuel_net=document.getElementById("annuel_net").value
     if (mensuel_net <= 1620){
         taux=0;
     } else if (mensuel_net <= 1682){
@@ -141,13 +143,16 @@ function calcul_taux() {
     document.getElementById("taux_text").innerHTML=taux;
     document.getElementById("taux").value=taux;
     document.getElementById("resultat").innerHTML=Math.round((100-taux)/100*mensuel_net);
+    document.getElementById("resultat2").innerHTML=Math.round((100-taux)/100*annuel_net);
 }
 
 function fn_taux() {
     taux=document.getElementById("taux").value;
     document.getElementById("taux_text").innerHTML=taux;
     const mensuel_net=document.getElementById("mensuel_net").value
+    const annuel_net=document.getElementById("annuel_net").value
     document.getElementById("resultat").innerHTML=Math.round((100-taux)/100*mensuel_net);
+    document.getElementById("resultat2").innerHTML=Math.round((100-taux)/100*annuel_net);
 }
 
 function fn_temps_travail() {
@@ -156,9 +161,16 @@ function fn_temps_travail() {
     horaire_brut();
 }
 
+function fn_prime() {
+    prime=document.getElementById("prime").value/12;
+    horaire_brut();
+}
+
 document.getElementById("temps_travail").value=temps_travail*100;
 document.getElementById("temps_travail_text").innerHTML=Math.round(temps_travail*100);
 fn_statut()
+fn_prime()
+
 document.getElementById("horaire_brut").addEventListener("keyup",horaire_brut);
 document.getElementById("mensuel_brut").addEventListener("keyup",mensuel_brut);
 document.getElementById("annuel_brut").addEventListener("keyup",annuel_brut);
@@ -168,3 +180,4 @@ document.getElementById("annuel_net").addEventListener("keyup",annuel_net);
 document.getElementById("statut").addEventListener("mouseup",fn_statut);
 document.getElementById("taux").addEventListener("mousemove",fn_taux);
 document.getElementById("temps_travail").addEventListener("mousemove",fn_temps_travail);
+document.getElementById("prime").addEventListener("mouseup",fn_prime);
